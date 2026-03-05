098a44d84338e97150d3efe1ad35f5445ddff55a
Kalpesh-V-pawar <Kalpeshpawar010401@gmail.com>
2026-03-05 09:20:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1071,7 +1071,7 @@ fetchNotes();
 def login_required(func):
     def wrapper(*args, **kwargs):
         if "username" not in session:
-        return redirect(url_for("Login_page"))  
+            return redirect(url_for("Login_page"))  
     wrapper.__name__ = func.__name__
     return wrapper
 
